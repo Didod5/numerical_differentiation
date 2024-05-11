7f3277ad1dfb0aca67e013a72e922125f1799db3
Dmitry <Didod1020@gmail.com>
2024-05-11 18:12:26 +0400

    deleted unnecessary comments

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print('Производная 4-го порядка =', df.fourth_derivative(f, 
 print('Оценка погрешности методом Рунге:', df.runge_rule(df.fourth_derivative, f, h, 4, x))
 
 
-# df.plot_error_vs_step(df.first_derivative, function=f, order=2, start=0, stop=9e-5, step=1e-6, x=x, deriv_name='1-го')
-# df.plot_error_vs_step(df.second_derivative, f, 2, 0, 9e-3, 1e-5, x, '2-го')
-# df.plot_error_vs_step(df.third_derivative, f, 4, 0, 1e-2, 1e-4, x, '3-го')
-# df.plot_error_vs_step(df.fourth_derivative, f, 4, 0, 1, 1e-3, x, '4-го')
+df.plot_error_vs_step(df.first_derivative, function=f, order=2, start=0, stop=9e-5, step=1e-6, x=x, deriv_name='1-го')
+df.plot_error_vs_step(df.second_derivative, f, 2, 0, 9e-3, 1e-5, x, '2-го')
+df.plot_error_vs_step(df.third_derivative, f, 4, 0, 1e-2, 1e-4, x, '3-го')
+df.plot_error_vs_step(df.fourth_derivative, f, 4, 0, 1, 1e-3, x, '4-го')
